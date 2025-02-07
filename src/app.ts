f4fe25623a0ccdaeb6336104b8ba6adaaba1da1b
MuhammadElshowehy <muhammadgamalelshowehy@gmail.com>
2025-02-08 00:40:32 +0200

    Enhance UI and validate form before submit.

--- src/app.ts
+++ src/app.ts
@@ -2,23 +2,76 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
       "project-input"
-    ) as HTMLTemplateElement;
-    this.hostElement = document.getElementById("app") as HTMLDivElement;
-    
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
     const importedNode = document.importNode(
       this.templateElement.content,
       true
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+
+  private validateUserData(): [string, string, number] | void {
+    const title = this.titleInputElement.value;
+    const desc = this.descriptionInputElement.value;
+    const people = this.peopleInputElement.value;
+    if (
+      title.trim().length === 0 ||
+      desc.trim().length === 0 ||
+      people.trim().length === 0 ||
+      parseInt(people.trim()) < 0
+    ) {
+      alert("Invalid inputs, please enter valid data!");
+    } else {
+      return [title, desc, +people];
+    }
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    const userData = this.validateUserData();
+    if (Array.isArray(userData)) {
+      const [title, desc, people] = userData;
+      console.log(title, desc, people);
+      this.resetForm();
+    }
+  }
+
+  private resetForm() {
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.peopleInputElement.value = "";
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
   }
 
   private attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.element);
   }
 }
-const projectInput = new ProjectInput();
+
+const prjInput = new ProjectInput();
